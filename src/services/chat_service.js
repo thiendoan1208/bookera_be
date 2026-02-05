dff3cecd351692700314ff60b02094cd5fd80b46
tannguyen888 <triumphdash00@gmai.com>
2026-02-05 01:26:02 -0500

    function send, edit, get messages

--- src/services/chat_service.js
+++ src/services/chat_service.js
@@ -6,6 +6,7 @@ const user = require("../models/user");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const db = require("../models");
 
 
 
@@ -27,6 +28,7 @@ io.on("connection", (socket) => {
 const UserConversation = require("../models/userconversation");
 const Conversation = require("../models/conversation");
 const User = require("../models/user");
+const e = require("express");
 
 exports.addUserToConversation = async (userId, conversationId) => {
   return UserConversation.create({
@@ -82,8 +84,61 @@ exports.isUserInConversation = async (userId, conversationId) => {
 };
 
 //tính năng thêm 
-exports.sendMessage = async ({ conversationId, senderId, content, type }) => {}
-exports.editMessage = async (messageId, userId, newContent) => {}
+exports.sendMessage = async ({ conversationId, senderId, content, type }) => {
+try{
+if(!content || content.trim() === '' || typeof content !== 'string'){
+  throw new Error("Invalid message content");
+}
+
+const insertMessage = await db.Message.create(
+    `INSERT INTO messages (user_id, conversation_id, content, created_at)
+       VALUES ($1, $2, $3, NOW()) RETURNING *`,
+      [senderId, conversationId, content.trim()]
+); 
+
+
+io.emit("newMessage", {  conversationId,
+  message: insertMessage,
+});
+}catch(err){
+  console.error('Error handling message:', err);
+    throw err;
+}
+}
+
+exports.getMessages = async (conversationId,page = 1, limit = 20) => {
+  const offset = (page - 1) * limit;  //dùng để giới hạn số trang trả về giảm tải db
+  const messages = await db.Message.create(
+  `SELECT * FROM messages
+   WHERE conversation_id = $1
+   ORDER BY created_at DESC
+   LIMIT $2 OFFSET $3`,
+  [conversationId, limit, offset]
+  );
+  return messages;
+}
+
+
+exports.editMessage = async (messageId, userId, newContent) => {
+  if(userId !== role.userId){
+    throw new Error("Unauthorized to edit this message");
+  }else{
+    const limit = 10; // giới hạn số lần chỉ đc 10 lần 
+
+    if(limit <= 0){
+    const updatedMessage = await db.Message.update(
+      `UPDATE messages
+       SET content = $1, updated_at = NOW()
+       WHERE id = $2 RETURNING *`,
+      [newContent, messageId]
+    );
+    limit--;
+    return updatedMessage;
+  }
+  
+  }
+    return updatedMessage;
+}
 exports.deleteMessage = async (messageId, userId) => {}
 exports.replyMessage = async ({ parentId, senderId, content }) => {}
 exports.forwardMessage = async ({ originalMessageId, newConversationId, senderId }) => {}
